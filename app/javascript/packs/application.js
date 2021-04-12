fbbf3f95235dcf3e4cf1e0ac63c7705f96915733
Josh Lee <lee45276@gmail.com>
2021-04-12 18:12:24 +0800

    Add semantic ui and jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import jquery from 'jquery'
 import "channels"
 
 Rails.start()
